a7f4f5d5fbada9c53736de95a087e406dd213ff0
Vakentina <midnanakamura@gmail.com>
2020-06-19 21:21:27 -0300

    Criando o projeto Atividade05

---
+++ Atividades/Atividade05/Assets/Scripts/PhotonConnection.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Runtime;
+PhotonNetwork.ConnectUsingSettings
+
+public class PhotonConnection : MonoBehaviourPunCallbacks
+{
+
+    public string gameVersion = "0.0.1";
+    public string nickname = "Player";
+
+    
+    void Start()
+    {
+        Debug.Log("Conectando ao servidor...", this);
+
+        PhotonNetwork.GameVersion = gameVersion;
+        PhotonNetwork.NickName = nickName + Random.Range(0, 9999);
+
+        
+}
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Conectado!", this);
+
+        if (PhotonNetwork.CountOfRooms == 0)
+        {
+            PhotonNetwork.JoinRoom(roomName);
+        }
+        else
+        {
+            RoomOptions options = new RoomOptions();
+            options.MaxPlayers = 4;
+            PhotonNetwork.JoinOrCreateRoom(roomName, options, TypedLobby.Default);
+        }
+
+    }
+    public override void OnDisconnected(Photon.Realtime.DisconnectCause cause)
+{
+    Debug.Log("Desconectado!");
+}
+
+
+void Update()
+    {
+        
+    }
+}
